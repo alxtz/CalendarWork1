7c2a7915931d9eab058e112aea62b42f3832461e
Bj <fate412460@gmail.com>
2016-07-06 21:01:30 +0800

    不是完全版

--- app/src/main/java/com/example/bj/calendarwork1/MainActivity.java
+++ app/src/main/java/com/example/bj/calendarwork1/MainActivity.java
@@ -42,6 +42,9 @@ public class MainActivity extends AppCompatActivity
         setCalendarRow1();
         setCalendarRow2();
         setCalendarRow3();
+        setCalendarRow4();
+        setCalendarRow5();
+        setCalendarRow6();
     }
 
     public void getStatusBarHeight()
@@ -213,6 +216,141 @@ public class MainActivity extends AppCompatActivity
         dayBlock6.setLayoutParams(params6);
 
         RelativeLayout dayBlock7 = (RelativeLayout) findViewById(R.id.DayBlock21);
+        ViewGroup.LayoutParams params7 = dayBlock7.getLayoutParams();
+        params7.height = dayBlockHeight;
+        params7.width = screenWidth-dayBlockWidth*6;
+        dayBlock7.setLayoutParams(params7);
+    }
+
+    public void setCalendarRow4()
+    {
+        RelativeLayout dayBlock1 = (RelativeLayout) findViewById(R.id.DayBlock22);
+        ViewGroup.LayoutParams params1 = dayBlock1.getLayoutParams();
+        params1.height = dayBlockHeight;
+        params1.width = dayBlockWidth;
+        dayBlock1.setLayoutParams(params1);
+
+        RelativeLayout dayBlock2 = (RelativeLayout) findViewById(R.id.DayBlock23);
+        ViewGroup.LayoutParams params2 = dayBlock2.getLayoutParams();
+        params2.height = dayBlockHeight;
+        params2.width = dayBlockWidth;
+        dayBlock2.setLayoutParams(params2);
+
+        RelativeLayout dayBlock3 = (RelativeLayout) findViewById(R.id.DayBlock24);
+        ViewGroup.LayoutParams params3 = dayBlock3.getLayoutParams();
+        params3.height = dayBlockHeight;
+        params3.width = dayBlockWidth;
+        dayBlock3.setLayoutParams(params3);
+
+        RelativeLayout dayBlock4 = (RelativeLayout) findViewById(R.id.DayBlock25);
+        ViewGroup.LayoutParams params4 = dayBlock4.getLayoutParams();
+        params4.height = dayBlockHeight;
+        params4.width = dayBlockWidth;
+        dayBlock4.setLayoutParams(params4);
+
+        RelativeLayout dayBlock5 = (RelativeLayout) findViewById(R.id.DayBlock26);
+        ViewGroup.LayoutParams params5 = dayBlock5.getLayoutParams();
+        params5.height = dayBlockHeight;
+        params5.width = dayBlockWidth;
+        dayBlock5.setLayoutParams(params5);
+
+        RelativeLayout dayBlock6 = (RelativeLayout) findViewById(R.id.DayBlock27);
+        ViewGroup.LayoutParams params6 = dayBlock6.getLayoutParams();
+        params6.height = dayBlockHeight;
+        params6.width = dayBlockWidth;
+        dayBlock6.setLayoutParams(params6);
+
+        RelativeLayout dayBlock7 = (RelativeLayout) findViewById(R.id.DayBlock28);
+        ViewGroup.LayoutParams params7 = dayBlock7.getLayoutParams();
+        params7.height = dayBlockHeight;
+        params7.width = screenWidth-dayBlockWidth*6;
+        dayBlock7.setLayoutParams(params7);
+    }
+
+    public void setCalendarRow5()
+    {
+        RelativeLayout dayBlock1 = (RelativeLayout) findViewById(R.id.DayBlock29);
+        ViewGroup.LayoutParams params1 = dayBlock1.getLayoutParams();
+        params1.height = dayBlockHeight;
+        params1.width = dayBlockWidth;
+        dayBlock1.setLayoutParams(params1);
+
+        RelativeLayout dayBlock2 = (RelativeLayout) findViewById(R.id.DayBlock30);
+        ViewGroup.LayoutParams params2 = dayBlock2.getLayoutParams();
+        params2.height = dayBlockHeight;
+        params2.width = dayBlockWidth;
+        dayBlock2.setLayoutParams(params2);
+
+        RelativeLayout dayBlock3 = (RelativeLayout) findViewById(R.id.DayBlock31);
+        ViewGroup.LayoutParams params3 = dayBlock3.getLayoutParams();
+        params3.height = dayBlockHeight;
+        params3.width = dayBlockWidth;
+        dayBlock3.setLayoutParams(params3);
+
+        RelativeLayout dayBlock4 = (RelativeLayout) findViewById(R.id.DayBlock32);
+        ViewGroup.LayoutParams params4 = dayBlock4.getLayoutParams();
+        params4.height = dayBlockHeight;
+        params4.width = dayBlockWidth;
+        dayBlock4.setLayoutParams(params4);
+
+        RelativeLayout dayBlock5 = (RelativeLayout) findViewById(R.id.DayBlock33);
+        ViewGroup.LayoutParams params5 = dayBlock5.getLayoutParams();
+        params5.height = dayBlockHeight;
+        params5.width = dayBlockWidth;
+        dayBlock5.setLayoutParams(params5);
+
+        RelativeLayout dayBlock6 = (RelativeLayout) findViewById(R.id.DayBlock34);
+        ViewGroup.LayoutParams params6 = dayBlock6.getLayoutParams();
+        params6.height = dayBlockHeight;
+        params6.width = dayBlockWidth;
+        dayBlock6.setLayoutParams(params6);
+
+        RelativeLayout dayBlock7 = (RelativeLayout) findViewById(R.id.DayBlock35);
+        ViewGroup.LayoutParams params7 = dayBlock7.getLayoutParams();
+        params7.height = dayBlockHeight;
+        params7.width = screenWidth-dayBlockWidth*6;
+        dayBlock7.setLayoutParams(params7);
+    }
+
+    public void setCalendarRow6()
+    {
+        RelativeLayout dayBlock1 = (RelativeLayout) findViewById(R.id.DayBlock36);
+        ViewGroup.LayoutParams params1 = dayBlock1.getLayoutParams();
+        params1.height = dayBlockHeight;
+        params1.width = dayBlockWidth;
+        dayBlock1.setLayoutParams(params1);
+
+        RelativeLayout dayBlock2 = (RelativeLayout) findViewById(R.id.DayBlock37);
+        ViewGroup.LayoutParams params2 = dayBlock2.getLayoutParams();
+        params2.height = dayBlockHeight;
+        params2.width = dayBlockWidth;
+        dayBlock2.setLayoutParams(params2);
+
+        RelativeLayout dayBlock3 = (RelativeLayout) findViewById(R.id.DayBlock38);
+        ViewGroup.LayoutParams params3 = dayBlock3.getLayoutParams();
+        params3.height = dayBlockHeight;
+        params3.width = dayBlockWidth;
+        dayBlock3.setLayoutParams(params3);
+
+        RelativeLayout dayBlock4 = (RelativeLayout) findViewById(R.id.DayBlock39);
+        ViewGroup.LayoutParams params4 = dayBlock4.getLayoutParams();
+        params4.height = dayBlockHeight;
+        params4.width = dayBlockWidth;
+        dayBlock4.setLayoutParams(params4);
+
+        RelativeLayout dayBlock5 = (RelativeLayout) findViewById(R.id.DayBlock40);
+        ViewGroup.LayoutParams params5 = dayBlock5.getLayoutParams();
+        params5.height = dayBlockHeight;
+        params5.width = dayBlockWidth;
+        dayBlock5.setLayoutParams(params5);
+
+        RelativeLayout dayBlock6 = (RelativeLayout) findViewById(R.id.DayBlock41);
+        ViewGroup.LayoutParams params6 = dayBlock6.getLayoutParams();
+        params6.height = dayBlockHeight;
+        params6.width = dayBlockWidth;
+        dayBlock6.setLayoutParams(params6);
+
+        RelativeLayout dayBlock7 = (RelativeLayout) findViewById(R.id.DayBlock42);
         ViewGroup.LayoutParams params7 = dayBlock7.getLayoutParams();
         params7.height = dayBlockHeight;
         params7.width = screenWidth-dayBlockWidth*6;
